b9a41a697a269ba5f0609562d5a528026bc93c84
sina-lib <mszandian@aut.ac.ir>
2020-10-04 23:56:42 +0100

    create a simple sine signal and get fft simple

--- Audio_STFT.py
+++ Audio_STFT.py
@@ -1,8 +1,16 @@
-import scipy as sp
+from scipy.fftpack import fft
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 t = np.linspace(0,2.5,2500)
+y = np.sin(44*t) + 5 * np.sin(244 * t) + 0.2
+noise = np.random.rand(2500)
+yf = fft(y+noise)
 
+plt.subplot(211)
+plt.plot(t,y)
+plt.subplot(212)
+plt.plot(t,yf)
 
+plt.show()
 
